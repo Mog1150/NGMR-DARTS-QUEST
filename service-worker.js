2cb6dea14974697b5aa08bebf514ffd357a93b02
Mog1150 <f.ryousuke15@gmail.com>
2026-07-30 19:26:13 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = "ngmr-darts-v1";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  // 今は何もしない
+});
